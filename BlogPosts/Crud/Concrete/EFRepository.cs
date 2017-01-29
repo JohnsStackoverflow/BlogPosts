f919175ca36d869b98e1815822b4e7219ce23ce7
John Dwyer <John Dwyer>
2017-01-29 21:55:39 +0000

    Removed extra view

--- BlogPosts/Crud/Concrete/EFRepository.cs
+++ BlogPosts/Crud/Concrete/EFRepository.cs
@@ -2,6 +2,7 @@
 using Crud.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Web;
 
@@ -19,22 +20,41 @@ namespace Crud.Concrete
 
         public void Save(PostModel Post)
         {
-            if (Post.PostID == 0)
+            try
             {
-                context.Posts.Add(Post);
-            }
-            else
-            {
-                PostModel dbEntry = context.Posts.Find(Post.PostID);
-                if (dbEntry != null)
+                if (Post.PostID == 0)
                 {
-                    dbEntry.Heading = Post.Heading;
-                    dbEntry.PostBody = Post.PostBody;
-                    dbEntry.ImageDisplayName = Post.ImageDisplayName;
-                    dbEntry.ImagePath = Post.ImagePath;
+                    context.Posts.Add(Post);
                 }
+                else
+                {
+                    PostModel dbEntry = context.Posts.Find(Post.PostID);
+                    if (dbEntry != null)
+                    {
+                        dbEntry.Heading = Post.Heading;
+                        dbEntry.PostBody = Post.PostBody;
+                
+                    }
+                }
+                context.SaveChanges();
+
             }
-            context.SaveChanges();
+            catch (DbEntityValidationException ex)
+            {
+                // Retrieve the error messages as a list of strings.
+                var errorMessages = ex.EntityValidationErrors
+                        .SelectMany(x => x.ValidationErrors)
+                        .Select(x => x.ErrorMessage);
+
+                // Join the list to a single string.
+                var fullErrorMessage = string.Join("; ", errorMessages);
+
+                // Combine the original exception message with the new one.
+                var exceptionMessage = string.Concat(ex.Message, " The validation errors are: ", fullErrorMessage);
+
+                // Throw a new DbEntityValidationException with the improved exception message.
+                throw new DbEntityValidationException(exceptionMessage, ex.EntityValidationErrors);
+            }
         }
 
         public PostModel DeletePosts(int PostID)
--- BlogPosts/Crud/Models/PostModel.cs
+++ BlogPosts/Crud/Models/PostModel.cs
@@ -7,27 +7,31 @@ using System.Web.Mvc;
 
 namespace Crud.Models
 {
-    public class PostModel
+    public partial class PostModel
     {
         [Key]
         [HiddenInput(DisplayValue = false)]
         public int PostID { get; set; }
         [Required(ErrorMessage = "Heading is Required")]
+        [Display(Name = "Heading")]
         public string Heading { get; set; }
         [Required(ErrorMessage = "Body is Required")]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Body")]
         public string PostBody { get; set; }
         public string ImageDisplayName { get; set; }
         public string ImagePath { get; set; } //Temporarly here until I can get the ImageModel Method Working
-        public virtual ICollection<ImageModel> Images { get; set; }
+        //public virtual ICollection<ImageModel> Images { get; set; }
+        public IEnumerable<HttpPostedFileBase> File { get; set; }
     }
 
-    public class ImageModel
-    {
-        [Key]
-        public int ImageId { get; set; }
-        public string ImagePath { get; set; }
-        public virtual PostModel Post { get; set; }
-    }
+    //public class ImageModel
+    //{
+    //    [Key]
+    //    public int ImageId { get; set; }
+    //    public string ImagePath { get; set; }
+    //    public virtual PostModel Post { get; set; }
+    //}
 
     //public class CreatePostViewModel
     //{
